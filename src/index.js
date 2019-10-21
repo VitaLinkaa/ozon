196282119aa752992e467a7f66ee1a65bc9df99f
Vita <Linkaa117@gmail.com>
2019-10-21 10:19:48 +0300

    'Filter'

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@
 // ставим и убираем галочку в CHECKBOX
 // foreach
 
-const checkbox = document.querySelectorAll('.filter-check_checkbox');
 
-checkbox.forEach(element => {
-  element.addEventListener('change', function () {
-    if (this.checked) {
-      this.nextElementSibling.classList.add('checked');
-    } else {
-      this.nextElementSibling.classList.remove('checked');
-    }
+function toggleCheckbox() {
+  const checkbox = document.querySelectorAll('.filter-check_checkbox');
+
+  checkbox.forEach(element => {
+    element.addEventListener('change', function () {
+      if (this.checked) {
+        this.nextElementSibling.classList.add('checked');
+      } else {
+        this.nextElementSibling.classList.remove('checked');
+      }
+    });
   });
-});
-
+};
 // for
 // const checkbox = document.querySelectorAll('.filter-check_checkbox');
 
@@ -32,67 +34,144 @@ checkbox.forEach(element => {
 // КОРЗИНА - ОТКРЫТЬ И ЗАКРЫТЬ
 // -----------------------------------------------------------------------------
 
-const btnCart = document.getElementById('cart');
-// кнопка открытия корзины
-
-const openCart = document.querySelector('.cart');
-// сама корзина, где display = none
-
-const cartClose = document.querySelector('.cart-close');
-// закрываем корзину на крестик
+function toggleBasket() {
+  const btnCart = document.getElementById('cart'); // кнопка открытия корзины
+  const openCart = document.querySelector('.cart'); // сама корзина, где display = none
+  const cartClose = document.querySelector('.cart-close'); // закрываем корзину на крестик
 
   btnCart.addEventListener('click', () => {
     openCart.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
-    // чтобы body не скролился
+    document.body.style.overflow = 'hidden'; // чтобы body не скролился
   });
 
   cartClose.addEventListener('click', () => {
     openCart.style.display = 'none';
     document.body.style.overflow = '';
   });
+};
 
+function addCart() {
+  const cards = document.querySelectorAll('.goods .card');
+  // получаем все карточки в обертки .goods (только те, что на главной странице)
+  const cartWrapper = document.querySelector('.cart-wrapper');
+  // находим обертку корзины, куда должны попадать заказы
+  const cardEmpty = document.getElementById('cart-empty');
+  // находим подпись, что корзина пуста
+  const count = document.querySelector('.counter');
+  // счетчик в корзине
 
-// ДОБАВЛЕНИЕ ТОВАРОВ В КОРЗИНУ
-// -----------------------------------------------------------------------------
-
-const cards = document.querySelectorAll('.goods .card');
-// получаем все карточки в обертки .goods (только те, что на главной странице)
-
-const cartWrapper = document.querySelector('.cart-wrapper');
-// находим обертку корзины, куда должны попадать заказы
-
-const cardEmpty = document.getElementById('cart-empty');
-// находим подпись, что корзина пуста
-
-const count = document.querySelector('.counter');
-// счетчик в корзине
-
-  cards.forEach((card) =>{
+  cards.forEach((card) => {
     const btn = card.querySelector('.btn');
     btn.addEventListener('click', () => {
       const cardClone = card.cloneNode(true);
       cartWrapper.appendChild(cardClone);
       // делаем клонирование карты co всеми вложенными елементами, клон попадае в корзину
-      cardEmpty.remove();
       showData();
+
+      const removeBtn = cardClone.querySelector('.btn');
+      removeBtn.textContent = 'Удалить из корзины';
+      removeBtn.addEventListener('click', () => {
+        cardClone.remove();
+        showData();
+      });
     });
   });
-// перебираем все карточки, создаем переменную для кнопки добавления карточки в 
-// корзину, при нажатии на эту кнопу срабатывает обработчик - создаем клон карточки
-// со всеми вложеными элементами и присобачиваем как ребенка в обертку корзины.
-// Удаляем надписть  - "Корзина пуста"
-// Запускаем функцию подсчета товаров в корзине
+  // перебираем все карточки, создаем переменную для кнопки добавления карточки в 
+  // корзину, при нажатии на эту кнопу срабатывает обработчик - создаем клон карточки
+  // со всеми вложеными элементами и присобачиваем как ребенка в обертку корзины.
+  // Удаляем надписть  - "Корзина пуста"
+  // Запускаем функцию подсчета товаров в корзине
 
 
-// ФУНКЦИЯ - ПОДСЧЕТ И ВЫВОД К-ВА ТОВАРА В КОРЗИНЕ
-// -----------------------------------------------------------------------------
-function showData() {
-  const countCards = cartWrapper.querySelectorAll('.card');
-  count.textContent = countCards.length;
-  console.log(countCards.length);
-// Находим переменную количество карточек в обертке корзины
-// Создаем переменную счетчик (const count = document.querySelector('.counter'),
-// навешиваем метод textContent и вываливаем в него длинну(к-во элементов в корзине)
+  // ФУНКЦИЯ - ПОДСЧЕТ И ВЫВОД К-ВА ТОВАРА В КОРЗИНЕ
+  // -----------------------------------------------------------------------------
+  function showData() {
+    const countCards = cartWrapper.querySelectorAll('.card');
+    // Находим переменную количество карточек в обертке корзины
+    // Создаем переменную счетчик (const count = document.querySelector('.counter'),
+    // навешиваем метод textContent и вываливаем в него длинну(к-во элементов в корзине)
+
+    const cardsPrice = cartWrapper.querySelectorAll('.card-price');
+    // Находим цены в карточках
+
+    const cardsTotal = document.querySelector('.cart-total span');
+    // Находим total в cardWrapper
+    let total = 0;
+    count.textContent = countCards.length;
+    // навешиваем метод textContent и вываливаем в него длинну(к-во элементов в корзине)
+
+    cardsPrice.forEach((cardPrice) => {
+
+      let price = parseFloat(cardPrice.textContent);
+      // получаем цену каждой карточки товара
+      total += price;
+      console.log(total);
+    });
+
+    cardsTotal.textContent = total;
+
+    if (countCards.length !== 0) {
+      cardEmpty.remove();
+    } else {
+      cartWrapper.appendChild(cardEmpty);
+    }
+  }
+
+  // ПОДСЧЕТ ОБЩЕЙ СУММЫ В КОРЗИНЕ
+  // -----------------------------------------------------------------------------
+  // Получаем переменнную с ценной товара в карточке
+  // Переходи в функцию showData() 
+  // УДАЛЕНИЕ ТОВАРА ИЗ КОРЗИНЫ
+  // -----------------------------------------------------------------------------
+  // 
 }
 
+// ФУНКЦИЯ ПОИСКА ПО ФИЛЬТРE АКЦИОННЫХ ТОВАРОВ
+// -----------------------------------------------------------------------------
+//
+function filterSearch() {
+  const cards = document.querySelectorAll('.goods .card');
+  const discountCheckbox = document.getElementById('discount-checkbox');
+  const min = document.getElementById('min');
+  const max = document.getElementById('max');
+
+    discountCheckbox.addEventListener('click', () =>{
+      cards.forEach((card) => {
+        if (discountCheckbox.checked) {
+          if (!card.querySelector('.card-sale')){
+            card.parentNode.style.display = 'none';
+          }
+        } else {
+          card.parentNode.style.display = '';
+        }
+      });
+    });
+
+    // проверка по цене
+  function filterPrice () {
+    cards.forEach((card) => {
+      const cardPrice = card.querySelector('.card-price');
+      const price = parseFloat(cardPrice.textContent);
+      
+      if (min.value && (price < min.value) || (max.value && price > max.value)) {
+        card.parentNode.style.display = 'none';
+      } else {
+        card.parentNode.style.display = '';      
+
+      }
+    });
+  }
+
+  min.addEventListener('change', filterPrice);
+  max.addEventListener('change', filterPrice);
+
+}
+
+toggleCheckbox();
+toggleBasket();
+addCart();
+filterSearch();
+
+
+// ФИЛЬТР
+// -----------------------------------------------------------------------------
---
+++ src/index_1.js
@@ -0,0 +1,11 @@
+const checkbox = document.querySelectorAll('.filter-check_checkbox');
+
+checkbox.forEach(element => {
+  element.addEventListener('change', function () {
+    if (this.checked) {
+      this.nextElementSibling.classList.add('checked');
+    } else {
+      this.nextElementSibling.classList.remove('checked');
+    }
+  });
+});
