999f249170d7fe4c860a9ccab0618ab011330327
Vita <Linkaa117@gmail.com>
2019-10-21 12:03:27 +0300

    'search'

--- src/index.js
+++ src/index.js
@@ -116,14 +116,6 @@ function addCart() {
       cartWrapper.appendChild(cardEmpty);
     }
   }
-
-  // ПОДСЧЕТ ОБЩЕЙ СУММЫ В КОРЗИНЕ
-  // -----------------------------------------------------------------------------
-  // Получаем переменнную с ценной товара в карточке
-  // Переходи в функцию showData() 
-  // УДАЛЕНИЕ ТОВАРА ИЗ КОРЗИНЫ
-  // -----------------------------------------------------------------------------
-  // 
 }
 
 // ФУНКЦИЯ ПОИСКА ПО ФИЛЬТРE АКЦИОННЫХ ТОВАРОВ
@@ -134,6 +126,8 @@ function filterSearch() {
   const discountCheckbox = document.getElementById('discount-checkbox');
   const min = document.getElementById('min');
   const max = document.getElementById('max');
+  const search = document.querySelector('.search-wrapper_input');
+  const searchBtn = document.querySelector('.search-btn');
 
     discountCheckbox.addEventListener('click', () =>{
       cards.forEach((card) => {
@@ -147,7 +141,7 @@ function filterSearch() {
       });
     });
 
-    // проверка по цене
+// ФИЛЬТР ПО ЦЕНЕ -----------------------------------------
   function filterPrice () {
     cards.forEach((card) => {
       const cardPrice = card.querySelector('.card-price');
@@ -157,7 +151,6 @@ function filterSearch() {
         card.parentNode.style.display = 'none';
       } else {
         card.parentNode.style.display = '';      
-
       }
     });
   }
@@ -165,13 +158,29 @@ function filterSearch() {
   min.addEventListener('change', filterPrice);
   max.addEventListener('change', filterPrice);
 
+// ПОИСК -----------------------------------------
+
+  searchBtn.addEventListener('click', () => {
+    const searchText = new RegExp(search.value.trim(), 'i'); //trim(пробелы), 'i' убрать зависимость от регистра
+    cards.forEach((card) => {
+      const title = card.querySelector('.card-title');
+
+      if (!searchText.test(title.textContent)) {
+        card.parentNode.style.display = 'none';
+        // Метод test проверяет строку на наличие совпадений с регулярным выражением. Метод возвращает true, если совпадения были найдены.
+      } else {
+        card.parentNode.style.display = '';
+      }    
+    });
+    search.value = '';
+  });
+  // Создали searchText, в переменно поиска(ее value + не учитывать пробели trim().
+  // Перебрали карточки и в них нашли пенеменную загаловка карточки
+  // Условие - если регулярка не включает контент title, то
+  // не выводи ничего, если включает = выведи title 
 }
 
 toggleCheckbox();
 toggleBasket();
 addCart();
 filterSearch();
-
-
-// ФИЛЬТР
-// -----------------------------------------------------------------------------
